121643d7eedae75860cdc758bd9e5aedd3078490
StephenOwen <stephenowen682@yahoo.com>
2017-07-19 17:42:48 +0800

    about page styles adeed

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,9 @@
 var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
-
-// autoprefix task
-gulp.task('prefix', function () {
-  return gulp.src('css/main.css')
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('css/'));
-});
 
 // minify images task
 gulp.task('image', function () {
-  return gulp.src('img/*')
+  return gulp.src('img/*/*')
     .pipe(imagemin())
     .pipe(gulp.dest('img'));
 });
