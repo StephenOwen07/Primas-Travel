067b6839b73313f3fbb1f277bbb279f087b2b986
StephenOwen <stephenowen682@yahoo.com>
2017-07-27 14:36:48 +0800

    overlay mobile-nav added to about.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Mobile Overlay Nav
+var buttonOpen = document.querySelector('.nav--open');
+var buttonClose = document.querySelector('.nav--close');
+
+buttonOpen.addEventListener('click', function () {
+  document.querySelector('.overlay').style.height = '100%';
+});
+
+buttonClose.addEventListener('click', function () {
+  document.querySelector('.overlay').style.height = '0%';
+});
+
 $(document).ready(function () {
 
   // scroll top button
