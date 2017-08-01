05f342374696cc00a6b807f6033462d1aa561fde
StephenOwen <stephenowen682@yahoo.com>
2017-08-01 13:41:15 +0800

    added gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 /* 
   -- TOP LEVEL FUNCTIONS --
@@ -9,20 +10,37 @@ var uglify = require('gulp-uglify');
   gulp.watch - Watch files and folders for changes
   */
 
-
-// Minify JS
-gulp.task('minify', function () {
-  gulp.src('app/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+// Copy src files to docs
+gulp.task('copy', function () {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('docs'));
+  gulp.src('src/css/*.css')
+    .pipe(gulp.dest('docs/css'));
+  gulp.src('src/font/**')
+    .pipe(gulp.dest('docs/font'));
+  gulp.src('src/img/**')
+    .pipe(gulp.dest('docs/img'));
+  gulp.src('src/slick/**')
+    .pipe(gulp.dest('docs/slick'));
 });
 
 
 
+// Minify and concat JS
+gulp.task('scripts', function () {
+  gulp.src('src/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('docs/js'));
+});
+
 // Default task
-gulp.task('default', ['minify']);
+gulp.task('default', ['copy', 'scripts']);
 
 // Watch task
 gulp.task('watch', function () {
-  gulp.watch('app/js/*.js', ['minify;']);
+  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('src/css/*css', ['copy']);
+  gulp.watch('src/img/*.js', ['copy']);
+  gulp.watch('src/*.html', ['copy']);
 });
