56b5160a7ce5d2a08953d9851110b1d04d2074be
StephenOwen <stephenowen682@yahoo.com>
2017-08-31 11:54:23 +0800

    updated datepicker to restrict check-out date to after check-in date

--- js/destination.js
+++ js/destination.js
@@ -4,13 +4,28 @@ $(function () {
   var $tomorrowsDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
 
   $('#check-in').datepicker({
-    showOtherMonths: true,
-    minDate: 0
+    dateFormat: "dd-M-yy",
+    minDate: 0,
+    onSelect: function (date) {
+      var date2 = $('#check-in').datepicker('getDate');
+      date2.setDate(date2.getDate() + 1);
+      $('#check-out').datepicker('setDate', date2);
+      //sets minDate to dt1 date + 1
+      $('#check-out').datepicker('option', 'minDate', date2);
+    }
   });
 
   $('#check-out').datepicker({
-    showOtherMonths: true,
-    minDate: $tomorrowsDate
+    dateFormat: "dd-M-yy",
+    onClose: function () {
+      var dt1 = $('#check-in').datepicker('getDate');
+      console.log(dt1);
+      var dt2 = $('#check-out').datepicker('getDate');
+      if (dt2 <= dt1) {
+        var minDate = $('#check-out').datepicker('option', 'minDate');
+        $('#check-out').datepicker('setDate', minDate);
+      }
+    }
   });
 
   // set checkout placeholder = current date
